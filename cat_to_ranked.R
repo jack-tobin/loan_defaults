e47ae1468375309ea0ed12cfd29de44c6be96117
jtobin190 <52289562+jtobin190@users.noreply.github.com>
2020-04-22 16:05:33 -0400

    Delete cat_to_ranked.R

--- cat_to_ranked.R
+++
@@ -1,7 +0,0 @@
-# categorical to ranked numeric
-cat_to_ranked <- function(df, categories) 
-{
-    A <- factor(df, levels=names(categories))
-    values <- unname(categories)
-    return(values[A])
-}
