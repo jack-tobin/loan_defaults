6a78e2ad599c81f650c2db99873040265d45cb2a
jtobin190 <52289562+jtobin190@users.noreply.github.com>
2020-04-22 16:03:16 -0400

    Add files via upload

--- loans.R
+++ loans.R
@@ -1,38 +1,39 @@
 rm(list=ls())
 
-# set working directory where files are stored
+# set working directory
 setwd('~/Documents/projects/loans')
 
-# source related functions
-source('cat_to_ranked.R')
-source('cat_to_num.R')
-source('convert_dates.R')
-source('process_data.R')
-source('train_test_split.R')
-source('pca.R')
-source('install_pkgs.R')
+# source related functions - these are stored in the wd
+source('functions/cat_to_ranked.R')
+source('functions/cat_to_num.R')
+source('functions/convert_dates.R')
+source('functions/process_data.R')
+source('functions/train_test_split.R')
+source('functions/pca.R')
+source('functions/install_pkgs.R')
 
-# packages
+# packages we'll need for the analysis
 packages <- c('caret','tree','rpart','nnet','randomForest',
               'gbm','lars','tidyverse','ggplot2','e1071')
 install.pkgs(packages)
 
 # read in files from kaggle dataset
+# source: https://www.kaggle.com/husainsb/lendingclub-issued-loans
 train_f <- file.path('data/lc_loan.csv')
 loans <- read.csv(train_f, header=TRUE, sep=',', stringsAsFactors=FALSE) #, nrow=10000)
 
-# function to process data from start to finish
+# apply processing function to the raw data
 loans_scrubbed <- process_data(loans)
 
-# pca
+# apply principal component analysis
 loans.pca <- pca(loans_scrubbed)
 
-# apply train test split
+# apply train test split function to partition the data
 train_test <- train_test_split(loans.pca, 0.7)
 train_ <- train_test$train_data
 test_ <- train_test$test_data
 
-# begin prediction using logistic regression
+# begin binary classification using logistic regression
 model <- caret::train(
     loan_status ~ ., 
     data=train_, 
@@ -40,16 +41,15 @@ model <- caret::train(
     family='binomial'
 )
 
-# predict
+# predict using testing data
 pred <- predict(
     model,
     newdata=test_
 )
 
-# confusion matrix
+# create confusion matrix to show results
 conf_mat <- confusionMatrix(
     pred,
     test_$loan_status
 )
-
-conf_mat
+print(conf_mat)
