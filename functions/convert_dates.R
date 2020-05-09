6bddd5bff792cb04abd21a7086e3527cd7b9110c
jtobin190 <52289562+jtobin190@users.noreply.github.com>
2020-05-09 09:43:55 -0400

    Update convert_dates.R

--- functions/convert_dates.R
+++ functions/convert_dates.R
@@ -2,7 +2,7 @@
 # R's internal date datatype
 convert_dates <- function(x) 
 {
-    # take column, paste '01-' to teh front, convert to date
+    # take column, paste '01-' to the front, convert to date
     x <- x %>%
         paste0('01-', .) %>%
         as.Date('%d-%b-%Y')
