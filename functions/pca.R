381869b5d519c32f3144a68e6c63f4b4fe0a6651
jtobin190 <52289562+jtobin190@users.noreply.github.com>
2020-05-09 09:46:51 -0400

    Update pca.R

--- functions/pca.R
+++ functions/pca.R
@@ -12,12 +12,12 @@ pca <- function(data)
     X <- data[, !(names(data) %in% c('loan_status'))]    
     
     # apply PCA, convert to dataframe
-    X.pca <- prcomp(X[, 2:length(X[1, ])], center=TRUE, scale=TRUE)$x %>%
+    X_pca <- prcomp(X[, 2:length(X[1, ])], center=TRUE, scale=TRUE)$x %>%
         as.data.frame()
     
     # add id and status back to transformed data
-    data.pca <- cbind(X$id, y$loan_status, X.pca)
-    colnames(data.pca) <- c('id','loan_status', colnames(X.pca))    
+    data_pca <- cbind(X$id, y$loan_status, X_pca) %>%
+        'colnames<-'(c('id','loan_status', colnames(X_pca)))
     
-    return(data.pca)
+    return(data_pca)
 }
